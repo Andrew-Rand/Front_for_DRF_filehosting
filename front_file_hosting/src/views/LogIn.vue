e713b42e6f337265d014c79d2eec026ca7c69402
Your Name <you@example.com>
2021-11-17 09:18:28 +0300

    create form for sign up and log in

--- front_file_hosting/src/views/LogIn.vue
+++ front_file_hosting/src/views/LogIn.vue
@@ -1,6 +1,13 @@
 <template>
     <div class='login-page'>
         <h1>Log in</h1>
+        <form @submit.prevent='submitForm'>
+            <label>Username:</label>
+            <input type='username' name='username' v-model = 'username'> <br><br>
+            <label>Password:</label>
+            <input type='password' name='password' v-model = 'password'> <br><br>
+            <button type='submit'>Log in</button>
+        </form>
     </div>
 </template>
 
@@ -8,6 +15,33 @@
 import axios from 'axios'
 
 export default {
-    name: 'LogIn'
+    name: 'LogIn',
+    data() {
+        return{
+            username: '',
+            password: ''
+        }
+    },
+    methods:{
+        submitForm(e) {
+            axios.defaults.headers.common['Authorization'] = ''
+            localStorage.removeItem('access')
+
+            const formData = {
+                username: this.username,
+                password: this.password
+            }
+
+            axios
+                .post('/api/accounts/login/', formData)
+                .then(response => {
+                    console.log(response)
+
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        }
+    }
 }
 </script>
--- front_file_hosting/src/views/SignUp.vue
+++ front_file_hosting/src/views/SignUp.vue
@@ -1,6 +1,21 @@
 <template>
     <div class='sign-up-page'>
         <h1>Register a new user</h1>
+        <form @submit.prevent='submitForm'>
+            <label>Username:</label>
+            <input type='username' name='username' v-model = 'username'> <br><br>
+            <label>Email:</label>
+            <input type='email' name='email' v-model = 'email'> <br><br>
+            <label>First Name:</label>
+            <input type='first_name' name='first_name' v-model = 'first_name'> <br><br>
+            <label>Last name:</label>
+            <input type='last_name' name='last_name' v-model = 'last_name'> <br><br>
+            <label>Age:</label>
+            <input type='age' name='age' v-model = 'age'> <br><br>
+            <label>Password:</label>
+            <input type='password' name='password' v-model = 'password'> <br><br>
+            <button type='submit'>Create new user</button>
+        </form>
     </div>
 </template>
 
@@ -8,6 +23,37 @@
 import axios from 'axios'
 
 export default {
-    name: 'SignUp'
+    name: 'SignUp',
+    data() {
+        return {
+            'first_name': '',
+            'last_name': '',
+            'email': '',
+            'password': '',
+            'username': '',
+            'age': '',
+        }
+    },
+    methods: {
+        submitForm(e){
+            const formData = {
+                first_name: this.first_name,
+                last_name: this.last_name,
+                email: this.email,
+                password: this.password,
+                username: this.username,
+                age: this.age,
+            }
+            axios
+                .post('/api/accounts/register/', formData)
+                .then(response => {
+                    console.log(response)
+                    this.$router.push('/')
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        }
+    }
 }
 </script>
