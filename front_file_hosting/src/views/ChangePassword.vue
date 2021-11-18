7562721f5d3b2fd42cbb19a8214accbf1a27f5f8
Your Name <you@example.com>
2021-11-18 10:23:42 +0300

    Add functional for edit user detail, and change password

--- front_file_hosting/src/views/ChangePassword.vue
+++ front_file_hosting/src/views/ChangePassword.vue
@@ -1,6 +1,15 @@
 <template>
-    <div class='user-page'>
-        <h1>Change password</h1>
+    <div class='user_page'>
+        <h1>Change_password</h1>
+        <form @submit.prevent='submitForm'>
+            <label>Password:</label>
+            <input type='password' name='password' v-model = 'password'> <br><br>
+            <label>New Password:</label>
+            <input type='password' name='new_password' v-model = 'new_password'> <br><br>
+            <label>New Password repeat:</label>
+            <input type='password' name='new_password_repeated' v-model = 'new_password_repeated'> <br><br>
+            <button type='submit'>Change Password</button>
+        </form>
     </div>
 </template>
 
@@ -8,6 +17,31 @@
 import axios from 'axios'
 
 export default {
-    name: 'ChangePassword'
+    name: 'ChangePassword',
+    data() {
+        return {
+            'password': '',
+            'new_password': '',
+            'new_password_repeated': ''
+        }
+    },
+    methods: {
+        submitForm(){
+            const formData = {
+                password: this.password,
+                new_password: this.new_password,
+                new_password_repeated: this.new_password_repeated,
+            }
+            axios
+                .put('accounts/change_password/', formData)
+                .then(response => {
+                    console.log(response)
+                    this.$router.push('/login')
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        }
+    }
 }
 </script>
--- front_file_hosting/src/views/User.vue
+++ front_file_hosting/src/views/User.vue
@@ -3,6 +3,18 @@
         <h1>About you</h1>
         <span :src='userdata'>{{ user_data }}</span>
     </div>
+    <form @submit.prevent='submitForm'>
+        <label>Email:</label>
+        <input type='email' name='email' v-model = 'email'> <br><br>
+        <label>First Name:</label>
+        <input type='text' name='first_name' v-model = 'first_name'> <br><br>
+        <label>Last name:</label>
+        <input type='last_name' name='last_name' v-model = 'last_name'> <br><br>
+        <label>Age:</label>
+        <input type='age' name='age' v-model = 'age'> <br><br>
+        <button type='submit'>Change user data</button>
+    </form>
+    <button @click='navigateHtml'>Change password</button>
 </template>
 
 <script>
@@ -27,6 +39,10 @@ export default {
                     response => {
                         console.log(response)
                         this.user_data = response.data
+                        this.age = response.data.age
+                        this.email = response.data.email
+                        this.first_name = response.data.first_name
+                        this.last_name = response.data.last_name
                     }
                 )
                 .catch(
@@ -34,6 +50,26 @@ export default {
                         console.log(error)
                     }
                 )
+        },
+        submitForm(){
+            const formData = {
+                first_name: this.first_name,
+                last_name: this.last_name,
+                email: this.email,
+                age: this.age,
+            }
+            axios
+                .patch('accounts/profile/', formData)
+                .then(response => {
+                    console.log(response)
+                    this.$router.push('/user')
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        },
+        navigateHtml() {
+            window.location.href = '#/user/change_password'
         }
     }
 }
