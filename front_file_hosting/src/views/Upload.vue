bfe0c8d9d28ffdf37434d17441371596e040ea13
andrey <1234567>
2021-11-23 17:06:18 +0300

    add different_upload_types

--- front_file_hosting/src/views/Upload.vue
+++ front_file_hosting/src/views/Upload.vue
@@ -1,167 +1,151 @@
 <template>
-    <div class='container'>
-        <h1>Upload files</h1>
-    </div>
-    	<div class="upload">
-        <div class="dropzone" ref="dropzone">
-            <div class="dropzone-display">
-                <div class="p-5">
-                    <br>
-                    <h3>Drop or click here to upload your files</h3>
-                    <br>
-                </div>
-            </div>
-        </div>
+  <div class='container'>
+    <h1>Upload files</h1>
+  </div>
+  <div class='container'>
+  <input type="file" ref="file">
+  <input type='text' ref="description">
 
-        <uploading
-            v-for="(file, index) in files"
-            v-bind:key="file.file.uniqueIdentifier + index"
-            :file="file.file"
-            :status="file.status"
-            :progress="file.progress"
-            @cancel="cancelFile"
-        />
-	</div>
+  <!--    <button v-on:click="resumableUpload">Upload</button>-->
+  <uploading
+      v-for="(file, index) in files"
+      v-bind:key="file.file.uniqueIdentifier + index"
+      :file="file.file"
+      :status="file.status"
+      :progress="file.progress"
+      @cancel="cancelFile"
+  />
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
-
 import Resumable from './resumable.js'
 import Uploading from './UploadingHelper'
 
 export default {
-    components: {
-        Uploading
+  name: "ResumableUpload",
+  components: {
+    Uploading
+  },
+  data() {
+    return {
+      files: [],
+      r: false,
+      description: ''
+    }
+  },
+  methods: {
+    findFile(file) {
+      return this.files.find(item => item.file.uniqueIdentifier === file.uniqueIdentifier && item.status !== 'canceled') ?? {}
     },
-
-    data(){
-        return {
-            files: [],
-            r: false
+    cancelFile(file) {
+      this.findFile(file).status = 'canceled'
+      file.cancel()
+    },
+    resumableUpload() {
+      this.r = new Resumable({
+        target: 'http://127.0.0.1:1338/api/files/chunk-upload/',
+        maxChunkRetries: 1,
+        chunkSize: 50 * 1024 * 1024,
+        simultaneousUploads: 4,
+        testChunks: true,
+        throttleProgressCallbacks: 1,
+        query: {
+          resumableHash: 'none',
+          resumableDescription: 'none'
         }
-    },
-    methods: {
+      });
+      this.r.assignBrowse(this.$refs.file);
+      this.r.assignDrop(this.$refs.file);
+      this.r.on('fileAdded', (file) => {
+        try {
 
-        findFile(file){
-            return this.files.find(item => item.file.uniqueIdentifier === file.uniqueIdentifier && item.status !== 'canceled') ?? {}
-        },
-
-        cancelFile(file){
-            this.findFile(file).status = 'canceled'
-            file.cancel()
-        }
-    },
-    mounted(){
-
-		this.r = new Resumable({
-			target:'http://127.0.0.1:1338/api/files/chunk-upload/',
-            maxChunkRetries: 1,
-            chunkSize:50*1024*1024,
-            simultaneousUploads:4,
-            testChunks:true,
-            throttleProgressCallbacks:1
-		});
-
-		if(!this.r.support) return alert('Your browser doesn\'t support chunked uploads. Get a better browser.');
-        this.r.assignBrowse(this.$refs.dropzone);
-		this.r.assignDrop(this.$refs.dropzone);
-
-        this.r.on('fileAdded', (file, event) => {
+          if (file.size > 50 * 1024 * 1024) {
             file.hasUploaded = false
-            console.log('this.files', this.files)
-
             this.files.push({
-                file,
-                status: 'uploading',
-                progress: 0
+              file,
+              status: 'uploading',
+              progress: 0
             })
             this.r.upload()
-        })
-        this.r.on('fileSuccess', (file, event) => {
-            this.findFile(file).status = 'success'
+          } else {
+            let formData = new FormData();
+            formData.append('file', file.file)
+            formData.append('total_size', file.size)
+            formData.append('type', file.file.type)
+            formData.append('filename', file.fileName)
+            formData.append('hash_sum', 'cab9279871936b5c987531c5f1de1a15')
+            formData.append('description', this.$refs.description.value)
 
-            delete axios.defaults.headers.common["Authorization"];
-            axios({
-                    url: 'http://127.0.0.1:1338/api/files/build/?' + 'resumableChunkNumber=1&resumableChunkSize=52428800&resumableCurrentChunkSize=52428800&resumableTotalSize=134217728&resumableType=text%2Fplain&resumableIdentifier=' +file.uniqueIdentifier+ '&resumableFilename=' +file.fileName+ '&resumableRelativePath=128_mb_file_text_new.txt&resumableTotalChunks=' + file.chunks.length  ,
-                    method: 'POST',
+            delete axios.defaults.headers.common['Authorization'];
+            axios.post('http://127.0.0.1:1338/api/files/file-upload/', formData)
+                .then(resp => {
+                  console.log(resp.data)
                 })
+                .catch(e => {
+                  let errorStatus = e.message.substr(e.message.length - 3);
+                  if (errorStatus === '403') {
+                    this.$router.push('/login')
+
+                  } else {
+                    console.log(errorStatus)
+                  }
+                })
+          }
+        } catch (error) {
+          this.$router.push('/login')
+
+        }
+      })
+
+      this.r.on('fileSuccess', (file) => {
+        this.findFile(file).status = 'success'
+        delete axios.defaults.headers.common["Authorization"];
+        this.description = this.$refs.description.value
+        if (this.description !== '') {
+          this.description = 'none'
+        }
+        axios({
+          url: 'http://127.0.0.1:1338/api/files/build/?' + 'resumableChunkNumber=1&resumableChunkSize=52428800&resumableCurrentChunkSize=52428800&resumableTotalSize=134217728&resumableType=text%2Fplain&resumableIdentifier=' +file.uniqueIdentifier+ '&resumableFilename=' +file.fileName+ '&resumableRelativePath=128_mb_file_text_new.txt&resumableTotalChunks=' + file.chunks.length  ,
+          method: 'POST',
+        })
                 .then(response => {
                         console.log(response)
                     }
                 )
-                .catch(
-                    error => {
-                        console.log(error)
-                        alert(error)
-                    }
-                )
-        })
-        this.r.on('fileError', (file, event) => {
-            this.findFile(file).status = 'error'
-        })
-        this.r.on('fileRetry', (file, event) => {
-            this.findFile(file).status = 'retrying'
-        })
-        this.r.on('fileProgress', (file) => {
-            // console.log('fileProgress', progress)
-            var localFile = this.findFile(file)
-
-            var progress = file.progress()
-            if( progress > localFile.progress)
-                localFile.progress = progress
-        })
-
-
-
-          XMLHttpRequest.prototype.open = (function(open) {
-            return function(method,url,async) {
-              open.apply(this,arguments);
-              this.setRequestHeader('Authorization', localStorage.getItem('access'));
-            };
-          })(XMLHttpRequest.prototype.open);
-
+            .catch(
+                error => {
+                  console.log(error)
+                  alert(error)
+                }
+            )
+      })
+      this.r.on('fileError', (file) => {
+        this.findFile(file).status = 'error'
+      })
+      this.r.on('fileRetry', (file) => {
+        this.findFile(file).status = 'retrying'
+      })
+      this.r.on('fileProgress', (file) => {
+        var localFile = this.findFile(file)
+        var progress = file.progress()
+        if (progress > localFile.progress)
+          localFile.progress = progress
+      })
+      XMLHttpRequest.prototype.open = (function (open) {
+        return function () {
+          open.apply(this, arguments);
+          this.setRequestHeader('Authorization', localStorage.getItem('access'));
+        };
+      })(XMLHttpRequest.prototype.open);
     }
+  },
+  mounted() {
+    this.resumableUpload()
+  }
 }
-
-
 </script>
+<style>
 
-<style lang="scss">
-.p-5 {
-	padding: 3rem;
-}
-.upload {
-	display: flex;
-	flex-direction: column;
-}
-.dropzone {
-    height: 340px;
-    width: 340px;
-    padding: 16px;
-	display: flex;
-	align-self: center;
-	margin-bottom: 1.2rem;
-    cursor: pointer;
-    * {
-        pointer-events: none;
-    }
-    .dropzone-display {
-        height: 100%;
-        width: 100%;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		flex-direction: column	;
-        border: 4px dashed #222;
-        border-radius: 32px;
-        img {
-            width: 64px;
-        }
-		small {
-			font-size: 0.65em;
-			display: block;
-		}
-    }
-}
 </style>
